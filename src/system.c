93a28548ee31b52821e8d7a5beb479b4a99c8c22
Knoll Dominik <Dominik.Knoll@newtec.de>
2026-09-02 11:53:24 +0200

    Formatted code

--- src/system.c
+++ src/system.c
@@ -2,7 +2,7 @@
  * system.c
  *
  * Created on: 23.04.2023
- * Autor: Dominik Knoll
+ * Author: Dominik Knoll
  *
  * Description:
  * System file for the NUCLEO-F411RE, without the use of the STM32 HAL.
@@ -23,37 +23,37 @@
  */
 void SystemInit(void)
 {
-	/* Enable HSE */
-	RCC->CR |= RCC_CR_HSEON;
-	/* Wait until HSE is stable */
-	while (!(RCC->CR & RCC_CR_HSERDY))
-		;
+    /* Enable HSE */
+    RCC->CR |= RCC_CR_HSEON;
+    /* Wait until HSE is stable */
+    while (!(RCC->CR & RCC_CR_HSERDY))
+        ;
 
-	/* Enable power interface clock */
-	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
-	/* Set regulator voltage scaling output section to Scale 1 mode <= 100 MHz */
-	PWR->CR |= 3 << PWR_CR_VOS_Pos;
+    /* Enable power interface clock */
+    RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+    /* Set regulator voltage scaling output section to Scale 1 mode <= 100 MHz */
+    PWR->CR |= 3 << PWR_CR_VOS_Pos;
 
-	/* Set flash access control register */
-	FLASH->ACR = FLASH_ACR_PRFTEN | FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_LATENCY_5WS;
+    /* Set flash access control register */
+    FLASH->ACR = FLASH_ACR_PRFTEN | FLASH_ACR_ICEN | FLASH_ACR_DCEN | FLASH_ACR_LATENCY_5WS;
 
-	/* Set AHB prescaler to /1 */
-	RCC->CFGR = RCC_CFGR_HPRE_DIV1;
-	/* Set APB1 prescaler to /2 */
-	RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
-	/* Set APB2 prescaler to /1 */
-	RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;
+    /* Set AHB prescaler to /1 */
+    RCC->CFGR = RCC_CFGR_HPRE_DIV1;
+    /* Set APB1 prescaler to /2 */
+    RCC->CFGR |= RCC_CFGR_PPRE1_DIV2;
+    /* Set APB2 prescaler to /1 */
+    RCC->CFGR |= RCC_CFGR_PPRE2_DIV1;
 
-	/* Set in RCC PLL configuration register PLL_M = /4, PLL_N = x100, PLL_P = /2 and PLLSRC to HSE*/
-	RCC->PLLCFGR = (4 << RCC_PLLCFGR_PLLM_Pos) | (100 << RCC_PLLCFGR_PLLN_Pos) | (2 << RCC_PLLCFGR_PLLP_Pos) | RCC_PLLCFGR_PLLSRC_HSE;
+    /* Set in RCC PLL configuration register PLL_M = /4, PLL_N = x100, PLL_P = /2 and PLLSRC to HSE*/
+    RCC->PLLCFGR = (4 << RCC_PLLCFGR_PLLM_Pos) | (100 << RCC_PLLCFGR_PLLN_Pos) | (2 << RCC_PLLCFGR_PLLP_Pos) | RCC_PLLCFGR_PLLSRC_HSE;
 
-	/* Set PLL ON in RCC clock control register */
-	RCC->CR |= RCC_CR_PLLON;
-	while (!(RCC->CR & RCC_CR_PLLRDY))
-		;
+    /* Set PLL ON in RCC clock control register */
+    RCC->CR |= RCC_CR_PLLON;
+    while (!(RCC->CR & RCC_CR_PLLRDY))
+        ;
 
-	/* Set the system clock switch (mux) to PLL in the RCC clock configuration register */
-	RCC->CFGR |= RCC_CFGR_SW_PLL;
-	while (!(RCC->CFGR & RCC_CFGR_SWS_PLL))
-		;
+    /* Set the system clock switch (mux) to PLL in the RCC clock configuration register */
+    RCC->CFGR |= RCC_CFGR_SW_PLL;
+    while (!(RCC->CFGR & RCC_CFGR_SWS_PLL))
+        ;
 }
